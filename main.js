1c850f4fced9181ee00918cb38d0d9e09cbbd4c7
你的英文名Lynnhan95 <han536@purdue.edu>
2019-02-10 14:13:05 -0500

    1

---
+++ main.js
@@ -0,0 +1,47 @@
+let n
+
+initialize()
+
+setInterval(
+()=>{
+
+    makeLeave(getImage(n))
+    .one('transitionend',(e)=>{makeEnter($(e.currentTarget))})
+
+    makeCurrent(getImage(n+1))
+    n +=1;
+}    
+  
+    ,3000)
+
+
+//functions below
+function initialize(){
+    n=1
+    $('.images> img:nth-child(1)').addClass('current')
+    .siblings().addClass('enter')
+}
+    
+function x(n){
+    if(n>3){
+        n = n%3
+        if(n === 0){
+            n=3
+        }
+    }
+    return n
+}
+function getImage(n){
+    return $('.images> img:nth-child('+x(n)+')')
+}
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+
+function makeLeave($node){
+    return $node.removeClass('enter current').addClass('leave')
+}
+
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
+}
